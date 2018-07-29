e5e3a780798e47f3cdf0f2f1222a33f349c2e751
Alisha Shahbazian <alishashahbazian@gmail.com>
2018-07-29 13:26:33 -0700

    first commit

---
+++ client/app.js
@@ -0,0 +1,100 @@
+// Chat Component
+const chatComponent = {
+    template: ` <div class="chat-box">
+                   <p v-for="data in content">
+                       <img v-bind:src=data.user.img class="circle" width="30px">
+                       <span><strong>{{data.user.name}}</strong> <small>{{data.date}}</small><span>
+                       <br />
+                       {{data.message}}
+                   </p>
+               </div>`,
+    props: ['content']
+}
+
+// Users Component
+const usersComponent = {
+    template: ` <div class="user-list">
+                   <h6>Active Users ({{users.length}})</h6>
+                   <ul v-for="user in users">
+                       <li>
+                            <img v-bind:src=user.img class="circle" width="30px">
+                            <span>{{user.name}}</span>
+                       </li>
+                       <hr>
+                   </ul>
+               </div>`,
+    props: ['users']
+}
+
+// Welcome Component
+const welcomeComponent = {
+    template: `<div class="me h5" style="text-align: center" v-if=user.name>
+                    <h4>Welcome</h4>
+                    <img class="circle" v-bind:src=user.img width=25%>
+                    <h5>{{user.name}}</h5>
+                </div>`,
+    props: ['user']
+}
+
+const socket = io()
+const app = new Vue({
+    el: '#chat-app',
+    data: {
+        loggedIn: false,
+        unsuccessful: false,
+        userName: '',
+        user: {},
+        users: [],
+        message: '',
+        messages: []
+    },
+    methods: {
+        joinUser: function () {
+            if (!this.userName)
+                return
+
+            socket.emit('join-user', this.userName)
+        },
+        sendMessage: function () {
+            if (!this.message)
+                return
+
+            socket.emit('send-message', { message: this.message, user: this.user })
+        }
+    },
+    components: {
+        'users-component': usersComponent,
+        'chat-component': chatComponent,
+        'welcome-component': welcomeComponent
+    }
+})
+
+
+// Client Side Socket Event
+socket.on('refresh-messages', messages => {
+    app.messages = messages
+})
+socket.on('refresh-users', users => {
+    app.users = users
+})
+
+socket.on('successful-join', user => {
+    // the successful-join event is emitted on all connections (open browser windows)
+    // so we need to ensure the loggedIn is set to true and user is set for matching user only
+    if (user.name === app.userName) {
+        app.user = user
+        app.loggedIn = true
+    }
+    app.unsuccessful = false
+    app.users.push(user)
+})
+
+socket.on('unsuccessful-join', user => {
+    app.unsuccessful = true
+})
+
+socket.on('successful-message', content => {
+    // clear the message after success send
+    app.message = ''
+    app.messages.push(content)
+})
